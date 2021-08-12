90a146ce7cf5b308fd4f74b474cdbb698b9c7331
aprietof <adrianprieto00@gmail.com>
2021-08-11 21:53:53 -0400

    Add about page

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis harum sequi quis repellendus hic dolores fugiat, accusamus similique dolorum quidem non blanditiis quae ex nihil, et placeat ducimus voluptas amet.</p>
+    </div>
+  )
+}
